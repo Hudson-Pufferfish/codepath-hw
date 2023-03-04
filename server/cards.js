38ec372d2f359f92e57cfae279aedd0c47818c2d
Hudson Nguyen <hungson.nguyen811@email.com>
2023-03-04 16:18:42 -0600

    added server info

---
+++ server/cards.js
@@ -0,0 +1,65 @@
+const cards =  [
+  {
+    id: 1,
+    level: "medium",
+    question: "What's the capital city of Hungary?",
+    answer: "The capital city of Hungary is Budapest.",
+  },
+  {
+    id: 2,
+    level: "hard",
+    question: "What's the capital city of Uzbekistan?",
+    answer: "The capital city of Uzbekistan is Tashkent.",
+  },
+  {
+    id: 3,
+    level: "easy",
+    question: "What's the capital city of South Korea?",
+    answer: "The capital city of Korea is Seoul",
+  },
+  {
+    id: 4,
+    level: "medium",
+    question: "What's the capital city of the Czech Republic?",
+    answer: "The capital city of the Czech Republic is Prague.",
+  },
+  {
+    id: 5,
+    level: "medium",
+    question: "What's the capital city of Australia?",
+    answer: "The capital city of Australia is Canberra.",
+  },
+  {
+    id: 6,
+    level: "medium",
+    question: "What's the capital city of Croatia?",
+    answer: "The capital city of Croatia is Zagreb.",
+  },
+  {
+    id: 7,
+    level: "easy",
+    question: "What's the capital city of Poland?",
+    answer: "The capital city of Poland is Warsaw.",
+  },
+  {
+    id: 8,
+    level: "hard",
+    question: "What's the capital city of Kyrgyzstan?",
+    answer: "The capital city of Kyrgyzstan is Bishkek.",
+  },
+  {
+    id: 9,
+    level: "medium",
+    question: "What's the capital city of Belgium?",
+    answer: "The capital city of Belgium is Brussels.",
+  },
+  {
+    id: 10,
+    level: "easy",
+    question: "What's the capital city of Portugal?",
+    answer: "The capital city of Portugal is Lisbon.",
+  },
+]
+
+
+export default cards;
