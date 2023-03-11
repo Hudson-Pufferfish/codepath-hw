1e23f56009bc31ffb3e4690c74e7e09c229c304b
Hudson Nguyen <hungson.nguyen811@email.com>
2023-03-11 16:20:08 -0600

    initiate the branch unit03

--- src/components/card/CardView.jsx
+++ src/components/card/CardView.jsx
@@ -7,34 +7,32 @@ const CardView = () => {
   const [card, setCard] = useState(CARD_DATA[0] ?? ''); 
   const [isFront, setFront] = useState(true);
 
-  const handleFlipCard = () => {
+  const handleFlipCard = useCallback(() => {
     setFront(isFront => !isFront)
-  }
+  }, [])
   
-  const handleSwitchCard = (dir) => {
+  const handleSwitchCard = useCallback((dir) => {
+    console.log(CARD_DATA);
     console.log('clicked');
     switch (dir) {
       case 'PREVIOUS':
-        // const prevCard = CARD_DATA.find(c => c.id === (card.id > 1 ? card.id - 1 : 1))
-        const prevCard = CARD_DATA[Math.floor(Math.random() * CARD_DATA.length)]
+        const prevCard = CARD_DATA.find(c => c.id === (card.id > 1 ? card.id - 1 : 1))
         setCard(prevCard)
         setFront(true)
         break;
       
       case 'NEXT':
-        // const nextCard = CARD_DATA.find(c => c.id === (card.id < CARD_DATA.length - 1 ? card.id + 1 : CARD_DATA.length - 1))
-        const nextCard = CARD_DATA[Math.floor(Math.random() * CARD_DATA.length)]
+
+        const nextCard = CARD_DATA.find(c => c.id === ((card.id < CARD_DATA.length - 1) ? card.id + 1 : CARD_DATA.length - 1))
         setCard(nextCard)
         setFront(true)
+        console.log(nextCard);
         break;
       
       default:
         return
     }
-  }
-
-  const handlePrevCard = useCallback(() => handleSwitchCard('PREVIOUS'), [])
-  const handleNextCard = useCallback(() => handleSwitchCard('NEXT'), [])
+  })
     
   return (
     <div>
@@ -45,25 +43,11 @@ const CardView = () => {
               <img srcSet={card.img ?? ''} alt="answer" />
             <p>{(card.answer ?? '')}</p>
           </div>
-          {/* {
-            (() => {
-              switch (isFront) {
-                case true:
-                  return <div className="flip-card-front"><p>{(card.question ?? '')}</p></div>
-                case false:
-                  return <div className="flip-card-back"><p>{(card.answer ?? '')}</p></div>
-                default:
-                  return null
-              }
-            })()
-          } */}
         </div>
       </div>
       <div className='dir-btn'>
-        {/* <Button onHandleCard={() => handleSwitchCard('PREVIOUS')}>Previous</Button> */}
-        {/* <Button onHandleCard={() => handleSwitchCard('NEXT')}>Next</Button> */}
-        <Button onHandleCard={handlePrevCard}>Previous</Button>
-        <Button onHandleCard={handleNextCard}>Next</Button>
+        <Button onHandleCard={() => handleSwitchCard('PREVIOUS')}>Previous</Button>
+        <Button onHandleCard={() => handleSwitchCard('NEXT')}>Next</Button>
       </div>
     </div>
   );
