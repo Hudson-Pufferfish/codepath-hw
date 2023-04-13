12dd0a8ba955c950e9b26cb5902bbb787eba4f00
Hudson Nguyen <hungson.nguyen811@email.com>
2023-04-13 13:25:04 -0500

    complete add and display supabase DONE and add gif

--- src/components/crewmates/CrewmateList.jsx
+++ src/components/crewmates/CrewmateList.jsx
@@ -1,21 +1,27 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback, useRef } from "react";
 import "./CrewmateList.css";
 import Crewmate from "./Crewmate";
 import { supabase } from "../../client";
 
 const CrewmateList = () => {
   const [crewmates, setCrewmates] = useState([]);
+  const isMounted = useRef(true);
 
   useEffect(() => {
-    async function fetchCrewmates() {
-      const { data, error } = await supabase.from("crewmate").select("id, name, speed, color");
-      if (error) {
-        console.error("Error fetching crewmates:", error.message);
-      } else {
-        setCrewmates(data);
-      }
-    }
+    isMounted.current = true;
     fetchCrewmates();
+    return () => {
+      isMounted.current = false;
+    };
+  }, []);
+
+  const fetchCrewmates = useCallback(async () => {
+    const { data, error } = await supabase.from("crewmate").select("id, name, speed, color");
+    if (error) {
+      console.error("Error fetching crewmates:", error.message);
+    } else {
+      if (isMounted.current) setCrewmates(data);
+    }
   }, []);
 
   return (
